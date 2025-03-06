9948cc58d18abbcf0a0bfb97e5f3178170a85a23
saki138 <tarotws@gmail.com>
2025-03-06 21:41:27 +0900

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+// ----- スライドショー -----
+
+$(document).ready(function(){
+    $('.slider').slick({
+      autoplay: true,//自動再生する：true しない：false
+      autoplaySpeed: 4000,//自動再生するスピード 4秒
+      dots: false,//ドット部分を表示する：true　しない：true
+      infinite: true,//無限にスライドする：true　しない：false
+      fade: true,//フェードする：true　スライドする：false
+      speed: 4000,//フェードするスピード 4秒
+      arrows: false,//矢印表示：true　矢印消す：false
+     })
+   })
+
+
+
+
+   
